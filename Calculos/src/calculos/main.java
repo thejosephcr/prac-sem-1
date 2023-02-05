94e917c8233361e90b35aa52a78d0c4ca87aadce
ADelgado-99 <angelodr44@gmail.com>
2023-02-05 11:15:01 -0600

    Feat: Inclusion de codigo del ejercicio b
    Se realiza el codigo del ejercicio b, se agrega comentarios y se integra al codigo ya hecho

--- Calculos/src/calculos/main.java
+++ Calculos/src/calculos/main.java
@@ -14,21 +14,33 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
+        //=============== Ejercicio b ===============       
+        Ejercicio_b sumaNumeros = new Ejercicio_b();    // Se llama a la clase Ejercicio_b, se le asigna el nombre del objeto y se iguala a new Ejercicio_b() para llamar al constructor
+
+        int dato = sumaNumeros.Sumanumeros(4); // Se pone el nombre del objeto . y el metodo creado y se le envia el valor de entrada
+        //Se llama
         
+        if (dato == -1) { // Se valida valor de entrada
+            System.out.println("=== [ERROR]: No es un numero natural ===");
+        } else {
+            System.out.printf("El resultado del ejercicio b es:%d\n", dato);
+
+        }
+
         //=============== Ejercicio d =============== 
         calculos calc = new calculos(); //Se crea objeto del ejercicio d
-        System.out.println("Resultado con ciclos");
+        System.out.println("Resultado con ciclos del ejercicio d:");
         System.out.println(calc.calcExponencial(2, 5)); //Imprime la información con ciclos
-        
-        double resultado = calc.calcExpo(2, 5);  
-        if(resultado == -1){
+
+        double resultado = calc.calcExpo(2, 5);
+        if (resultado == -1) {
             System.out.println("=== [ERROR] : Uno de los valores es 0 ===");
             System.out.println(resultado);
         } else {
-            System.out.println("\nResultado con recursividad");
+            System.out.println("\nResultado con recursividad del ejercicio d:");
             System.out.println(resultado); //Imprime la información con recursividad
         }
     }
-    
+
 }
